7f9537c51529e0f168873134c29a9f8dc586fa04
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-09-06 23:34:45 -0700

    update

--- src/routes/album/K/+page.svelte
+++ src/routes/album/K/+page.svelte
@@ -1,3 +1,16 @@
+<script>
+    export let data;
+    console.log(data.albumList);
 
+    let fuck = (a1) => {
+      alert(a1)
+    }
 
-<h1>this is page K</h1>
+  </script>
+
+  {#each data.albumList as alb}
+
+      <h1 on:click={fuck(alb[1])}>{alb[0]}</h1>
+      <!-- <h1>{alb[1]}</h1> -->
+
+  {/each}
--- src/routes/artist/K/+page.svelte
+++ src/routes/artist/K/+page.svelte
@@ -1,3 +1,16 @@
+<script>
+  export let data;
+  console.log(data.artistList);
 
+  let fuck = (a1) => {
+    alert(a1)
+  }
 
-<h1>this is page K</h1>
+</script>
+
+{#each data.artistList as art}
+
+    <h1 on:click={fuck(art[1])}>{art[0]}</h1>
+    <!-- <h1>{art[1]}</h1> -->
+
+{/each}
